0033115e3af994f477614da33b7b48481a998724
Dinmaprecious <egbuchidinmaprecious@example.com>
2022-07-01 19:11:20 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,23 +9,18 @@
  **/
 int main(void)
 {
-/**
- *
- * hexadecimal
- * 0 1 2 3 4 5 6 7 8 9 a c d e f
- */
-char ch;
-int n;
-
-for (n = 48; n <= 57; n++)
+char num = '0';
+while (num <= '9')
 {
-putchar(n);
+putchar(num);
+num++;
 }
-for (ch = 'a'; ch <= 'f'; ch++)
+num = 'a';
+while (num <= 'f')
 {
-putchar(ch);
+putchar(num);
+num++;
 }
-putchar(10);
-
+putchar('\n');
 return (0);
 }
