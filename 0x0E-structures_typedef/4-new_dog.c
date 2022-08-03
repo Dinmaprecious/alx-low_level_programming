b041425496edfd2a9dc3c5c3894272b44e9ad4fd
Dinmaprecious <egbuchidinmaprecious@example.com>
2022-08-03 07:05:10 -0700

    Task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -70,7 +70,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (dog == NULL)
 		return (NULL);
 
-	free(dog);
+	dog->name = malloc(sizeof(char) * (len1 + 1));
+	if (dog->name == NULL)
+	{
+		free(dog);
 		return (NULL);
 	}
 	dog->owner = malloc(sizeof(char) * (len2 + 1));
@@ -86,3 +89,5 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	return (dog);
 }
+
+
